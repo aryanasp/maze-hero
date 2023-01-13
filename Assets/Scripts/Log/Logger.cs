57400c71dc07989a30afa577965bb1d202ccb503
Aryana Peyrovi <aryana761227@gmail.com>
2023-01-13 06:03:03 +0330

    Add logger class.

---
+++ Assets/Scripts/Log/Logger.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Log
+{
+    public static class Logger
+    {
+        private static LoggerConfig _loggerConfig;
+        static Logger()
+        {
+            _loggerConfig = Resources.Load<LoggerConfig>("Log/LoggerConfig");
+        }
+
+        public static void Log(string message, bool forceLog = false)
+        {
+            if (forceLog)
+            {
+                Debug.Log(message);
+            }
+            if (_loggerConfig.log)
+            {
+                Debug.Log(message);
+            }
+        }
+
+
+        public static void Warning(string message, bool forceLog = false)
+        {
+            if (forceLog)
+            {
+                Debug.LogWarning(message);
+            }
+            if (_loggerConfig.log)
+            {
+                Debug.LogWarning(message);
+            }
+        }
+        
+        public static void Error(string message, bool forceLog = false)
+        {
+            if (forceLog)
+            {
+                Debug.LogError(message);
+            }
+            if (_loggerConfig.log)
+            {
+                Debug.LogError(message);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Log/LoggerConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Log
+{
+    [CreateAssetMenu(fileName = "LoggerConfig", menuName = "Game/Monitor/Logger Config", order = 0)]
+    public class LoggerConfig : ScriptableObject
+    {
+        public bool log;
+        public bool warnings;
+        public bool errors;
+        public bool exceptions;
+    }
+}

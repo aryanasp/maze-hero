ea56b7485ba13c96853ab6d0f4e361495269d6cb
Aryana Peyrovi <aryana761227@gmail.com>
2023-01-13 06:47:27 +0330

    Disable score force log.

--- Assets/Scripts/Character/Behaviours/CharacterApple.cs
+++ Assets/Scripts/Character/Behaviours/CharacterApple.cs
@@ -11,7 +11,7 @@ namespace Character
 
         private void Update()
         {
-            Logger.Log($"[CharacterApple]: Score {_scoreModel.Score}", true);
+            Logger.Log($"[CharacterApple]: Score {_scoreModel.Score}");
         }
 
         private void OnTriggerEnter2D(Collider2D col)

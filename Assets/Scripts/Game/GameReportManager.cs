60901746d1b89cd95326867022859e7f152a9a33
Aryana Peyrovi <aryana761227@gmail.com>
2023-01-28 00:34:01 +0330

    Add report name.

--- Assets/Scripts/Game/GameReportManager.cs
+++ Assets/Scripts/Game/GameReportManager.cs
@@ -2,6 +2,8 @@
 using System.Globalization;
 using System.IO;
 using System.Text;
+using Ai;
+using UnityEngine;
 using Zenject;
 
 namespace Game
@@ -10,6 +12,7 @@ namespace Game
     {
         private readonly GameStatModel _gameStatModel;
         private readonly GameConfig _gameConfig;
+        private readonly GeneticAlgorithmConfig _geneticAlgorithmConfig;
 
         public void Dispose()
         {
@@ -17,10 +20,12 @@ namespace Game
         }
 
         [Inject]
-        public GameReportManager(GameStatModel gameStatModel, GameConfig gameConfig)
+        public GameReportManager(GameStatModel gameStatModel, GameConfig gameConfig,
+            GeneticAlgorithmConfig geneticAlgorithmConfig)
         {
             _gameStatModel = gameStatModel;
             _gameConfig = gameConfig;
+            _geneticAlgorithmConfig = geneticAlgorithmConfig;
             _gameStatModel.OnGameFinish += DoReport;
         }
 
@@ -30,15 +35,31 @@ namespace Game
             {
                 return;
             }
+
+            var reportName =
+                $"__Report_" +
+                $"_RoundCount_{_gameConfig.roundCount}_" +
+                $"_RoundDuration_{_gameConfig.roundDuration}_" +
+                $"_InitialPopulation_{_geneticAlgorithmConfig.initialPopulation}_" +
+                $"_Heuristic_{_geneticAlgorithmConfig.initialHeuristic}_" +
+                $"_AlgorithmType_{_geneticAlgorithmConfig.geneticAlgorithmType}_" +
+                $"_PersonMutate_{_geneticAlgorithmConfig.chanceToMutateNewPerson}_" +
+                $"_GeneMutate_{_geneticAlgorithmConfig.geneMutationChance}_" +
+                $"_UpdateEpochTime_{_geneticAlgorithmConfig.updateEpochTime}_";
             var stringBuilder = new StringBuilder();
             //TODO: Generate a CSV file by indexing round number and inputs in game config and genetic algorithm config
-            using var writer = new StreamWriter("Assets/Resources/Report.csv");
+            using var writer = new StreamWriter($"{Application.persistentDataPath}/{reportName}.csv");
             stringBuilder.AppendLine(
                 "Round Number, Apples Eaten, Max Score Fragment, Higher Than Average People Fragment, Max Score To Average Fragment");
             foreach (var keyValue in _gameStatModel.RoundsStat)
             {
-                stringBuilder.AppendLine($"{keyValue.Key}, {keyValue.Value.appleEaten}, {keyValue.Value.maxScoreToApplesTotalScoreFragment * 100d}, {keyValue.Value.percentageOfHigherThanAveragePeople * 100d}, {keyValue.Value.maxScoreToAverageFragment * 100d}");
+                stringBuilder.AppendLine(
+                    $"{keyValue.Key}, {keyValue.Value.appleEaten}," +
+                    $" {(keyValue.Value.maxScoreToApplesTotalScoreFragment * 100f).ToString(CultureInfo.InvariantCulture)}," +
+                    $" {(keyValue.Value.percentageOfHigherThanAveragePeople * 100f).ToString(CultureInfo.InvariantCulture)}," +
+                    $" {(keyValue.Value.maxScoreToAverageFragment * 100f).ToString(CultureInfo.InvariantCulture)}");
             }
+
             writer.Write(stringBuilder.ToString());
         }
     }

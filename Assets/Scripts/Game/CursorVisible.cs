f73609c29c60839c675b226db61ac2876c88f0aa
Aryana Peyrovi <aryana761227@gmail.com>
2023-01-27 03:57:31 +0330

    Add cursor visible, Add some functionality to project setting like force sign instance, run in background.

---
+++ Assets/Scripts/Game/CursorVisible.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class CursorVisible
+    {
+        public CursorVisible()
+        {
+            Cursor.visible = true;
+        }
+    }
+}
--- Assets/Scripts/Installers/ProjectInstaller.cs
+++ Assets/Scripts/Installers/ProjectInstaller.cs
@@ -1,3 +1,4 @@
+using Game;
 using UnityEngine;
 using Zenject;
 
@@ -7,6 +8,7 @@ namespace Installers
     {
         public override void InstallBindings()
         {
+            Container.Bind<CursorVisible>().AsSingle();
         }
     }
 }

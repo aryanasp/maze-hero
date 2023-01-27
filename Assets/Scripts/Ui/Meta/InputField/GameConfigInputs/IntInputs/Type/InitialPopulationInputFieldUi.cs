42ff65ecdcc42056d274734fa8ba53409934e62c
Aryana Peyrovi <aryana761227@gmail.com>
2023-01-27 15:14:27 +0330

    Fix population bug by adding a validator that stops user from entering non-4-multiple number into population field.

--- Assets/Scripts/Ui/Meta/InputField/GameConfigInputs/IntInputs/Type/InitialPopulationInputFieldUi.cs
+++ Assets/Scripts/Ui/Meta/InputField/GameConfigInputs/IntInputs/Type/InitialPopulationInputFieldUi.cs
@@ -8,6 +8,16 @@
 
         }
 
+        protected override string OnInputValidate(string inpString)
+        {
+            var validatedString = base.OnInputValidate(inpString);
+            var number = int.Parse(validatedString);
+            if (number % 4 != 0)
+            {
+                number -= number % 4;
+            }
+            return number.ToString();
+        }
         protected override string GetDescription()
         {
             return "Initial Population";

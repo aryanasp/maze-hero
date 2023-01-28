c255130335c5037324647159181979de23ba7a20
Aryana Peyrovi <aryana761227@gmail.com>
2023-01-28 04:29:39 +0330

    Add default values to configs, Add quit popup to both scenes.

--- Assets/Scripts/Game/GameConfig.cs
+++ Assets/Scripts/Game/GameConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Character;
 using Sirenix.OdinInspector;
 using UnityEngine;
---
+++ Assets/Scripts/Ui/Core/Hud/QuitManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Ui.Core.Hud
+{
+    public class QuitManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject quitPopup;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                quitPopup.SetActive(!quitPopup.activeInHierarchy);
+            }
+        }
+
+        public void OnYes()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+        }
+
+        public void OnNo()
+        {
+            quitPopup.SetActive(false);
+        }
+    }
+}
